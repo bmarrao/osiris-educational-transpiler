8f6b5951d18c469e864c87ebfadad7b035271995
Brenin <marraob@gmail.com>
2024-12-11 14:58:40 +0000

    FInish web worker tests

--- ANTLR/src/worker.js
+++ ANTLR/src/worker.js
@@ -1,6 +1,15 @@
-// worker.js
-self.onmessage = function(event) {
-    const { counter } = event.data;
-    postMessage(counter + 1);  // Increment the counter and send it back to the main thread
-};
+let counter = 0;
 
+self.onmessage = function (event) {
+    const { type, value } = event.data;
+
+    if (type === "init") {
+        counter = value || 0; // Initialize counter with provided value or default to 0
+        postMessage(`Counter initialized to ${counter}`);
+    } else if (type === "increment") {
+        counter += 1; // Increment the counter
+        postMessage(`Counter incremented to ${counter}`);
+    } else {
+        postMessage("Unknown message type");
+    }
+};
