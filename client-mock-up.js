5972e9ecf81162eba6bd6a049f6cb9a7376592e3
bmarrao <marraob@gmail.com>
2025-01-01 14:39:11 +0000

    Cleaning up and leaivng only what was inside ANTLR

--- client-mock-up.js
+++
@@ -1,9 +0,0 @@
-const { transpilePython, transpileRust } = require('seu-pacote');
-
-const pythonCode = `print("Hello, World!")`;
-const jsCodeFromPython = transpilePython(pythonCode);
-console.log(jsCodeFromPython);
-
-const rustCode = `fn main() { println!("Hello, World!"); }`;
-const jsCodeFromRust = transpileRust(rustCode);
-console.log(jsCodeFromRust);
