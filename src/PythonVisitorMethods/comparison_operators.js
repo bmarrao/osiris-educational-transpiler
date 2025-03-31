996e662bbd927df314fec8ff7ed9e09902d40e79
bmarrao <marraob@gmail.com>
2025-03-31 23:27:51 +0100

    Fix

--- src/PythonVisitorMethods/comparison_operators.js
+++ src/PythonVisitorMethods/comparison_operators.js
@@ -13,7 +13,7 @@ export function visitComparison(ctx) {
     }
     let evalString = `${left} ${comparisonPairs.join(' ')}`.replace(/\s+/g, ' ').trim(); // Ensure single space between tokens
     if(comparisonPairs.length >= 1 ) {
-        return `osiris_builtin_python_evalPythonComparison('${evalString}', { ${this.localVars.map(v => v === "self" ? "self: this" : `${v}: ${v}`).join(", ")} })`;
+        return `osiris_builtin_python_evalPythonComparison('${evalString}', { ${this.localVars.map(v => v === "self" ? "this : this" : `${v}: ${v}`).join(", ")} })`;
 
     }
     else 
