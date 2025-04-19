ec143e053e2f75ac519a1f33d05b75ca09ea4811
bmarrao <marraob@gmail.com>
2025-04-19 21:31:38 +0100

    Fixing double comprehensions

--- src/Python/PythonVisitorMethods/comprehension_generators.js
+++ src/Python/PythonVisitorMethods/comprehension_generators.js
@@ -2,11 +2,12 @@ import { dealTargets } from '../tools/targets.js';
 
 export function visitFor_if_clauses(ctx) {
     let x = ctx.for_if_clause().map(clause => this.visit(clause));
-    console.log(`x is ${x}`)
-    return x
+    console.log(`x is ${x}`);
+    return x;
 }
+
 export function visitFor_if_clause(ctx) {
-    let targetsRet = dealTargets(this.visit(ctx.star_targets()),this.localVars) 
+    let targetsRet = dealTargets(this.visit(ctx.star_targets()), this.localVars);
     return {
         async: !!ctx.ASYNC(),
         targets: targetsRet,
@@ -15,73 +16,78 @@ export function visitFor_if_clause(ctx) {
     };
 }
 
+function buildNestedLoops(clauses, innerCode) {
+    let loopCode = innerCode;
+    for (const clause of clauses.reverse()) {
+        loopCode = `
+            for (let ${clause.targets} of ${clause.iterable}) {
+                ${clause.conditions.map(cond => `if (!(${cond})) continue;`).join('\n')}
+                ${loopCode}
+            }
+        `;
+    }
+    return loopCode;
+}
+
 export function visitListcomp(ctx) {
-//     console.log("Here")
     let length = this.localVars.length;
-    const [clause] = this.visit(ctx.for_if_clauses());
-    console.log(clause)
+    const clauses = this.visit(ctx.for_if_clauses());
     const expr = this.visit(ctx.named_expression());
     this.localVars.length = length;
+
+    const loopCode = buildNestedLoops(clauses, `_result.push(${expr});`);
+
     return `(() => {
         let _result = [];
-        for (let ${clause.targets} of ${clause.iterable}) {
-            ${clause.conditions.map(cond => `if (!(${cond})) continue;`).join("\n            ")}
-            _result.push(${expr});
-        }
+        ${loopCode}
         return _result;
     })()`;
 }
 
 export function visitSetcomp(ctx) {
-//     console.log("2Here")
     let length = this.localVars.length;
-    const [clause] = this.visit(ctx.for_if_clauses());
+    const clauses = this.visit(ctx.for_if_clauses());
     const expr = this.visit(ctx.named_expression());
     this.localVars.length = length;
 
+    const loopCode = buildNestedLoops(clauses, `_result.add(${expr});`);
+
     return `(() => {
         let _result = new Set();
-        for (let ${clause.targets} of ${clause.iterable}) {
-            ${clause.conditions.map(cond => `if (!(${cond})) continue;`).join("\n            ")}
-            _result.add(${expr});
-        }
+        ${loopCode}
         return _result;
     })()`;
 }
 
 export function visitGenexp(ctx) {
-//     console.log("4Here")
     let length = this.localVars.length;
-    const [clause] = this.visit(ctx.for_if_clauses());
+    const clauses = this.visit(ctx.for_if_clauses());
     const expr = ctx.assignment_expression()
-        ? this.visit(ctx.assignment_expression()) 
+        ? this.visit(ctx.assignment_expression())
         : this.visit(ctx.expression());
     this.localVars.length = length;
 
-        return `(() => {
+    const loopCode = buildNestedLoops(clauses, `yield ${expr};`);
+
+    return `(() => {
         function* generator() {
-            for (let ${clause.targets} of ${clause.iterable}) {
-                ${clause.conditions.map(cond => `if (!(${cond})) continue;`).join("\n                ")}
-                yield ${expr};
-            }
+            ${loopCode}
         }
         return generator();
     })()`;
 }
 
 export function visitDictcomp(ctx) {
-//     console.log("3Here")
     let length = this.localVars.length;
-    const [clause] = this.visit(ctx.for_if_clauses());
+    const clauses = this.visit(ctx.for_if_clauses());
     const [keyExpr, valueExpr] = this.visit(ctx.kvpair()).split(':').map(s => s.trim());
     this.localVars.length = length;
+
+    const loopCode = buildNestedLoops(clauses, `_result[${keyExpr}] = ${valueExpr};`);
+
     return `(() => {
         let _result = {};
-        for (let ${clause.targets} of ${clause.iterable}) {
-            ${clause.conditions.map(cond => `if (!(${cond})) continue;`).join("\n            ")}
-            _result[${keyExpr}] = ${valueExpr};
-        }
+        ${loopCode}
         return _result;
     })()`;
 }
-
